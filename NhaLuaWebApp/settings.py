810e005bc3a5c05e01b024c1ba4e38a7a82631de
minhduoc611 <minhduoc611@gmail.com>
2025-09-22 08:47:36 +0700

    Add Railway deployment files

--- NhaLuaWebApp/settings.py
+++ NhaLuaWebApp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%i(lpp4@w=%aej)*dt(j7=eplm$#@d1xl0%i-@h-8vjvq&eikd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,6 +76,7 @@ PANCAKE_API_BASE_URL = 'https://pos.pages.fm/api/v1'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn NhaLuaWebApp.wsgi --log-file -
